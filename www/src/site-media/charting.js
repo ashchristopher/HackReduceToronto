0a023cd66813e0fe1f79b029941fb8fbe2177ee6
bartek <bart.ciszk@gmail.com>
2011-06-18 18:07:57 -0400

    moar demo

--- www/src/site-media/charting.js
+++ www/src/site-media/charting.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
       tooltip: {
          formatter: function() {
             return ''+
-               this.series.name + ' (' + this.y +' queries)';
+               this.series.name + ' (' + this.y +' queries), (' + this.x + ')';
          }
       },
       plotOptions: {
