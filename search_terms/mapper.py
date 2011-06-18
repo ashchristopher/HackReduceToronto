30398fbc34c949ff25bbc48041d0495b1aacd075
Ash Christopher <ash.christopher@gmail.com>
2011-06-18 14:33:55 -0400

    update

--- search_terms/mapper.py
+++ search_terms/mapper.py
@@ -1,3 +1,19 @@
 def map(line, params):
-    for word in line.split():
-        yield word, 1
+    """
+    hackreduce:search:history format:
+        None, timestamp, id, search, frequency?
+    """
+    from datetime import datetime, timedelta
+    from disco.util import msg
+
+    try: 
+        unknown, timestamp, uid, query, frequency = line.split("','")
+    except ValueError:
+        print msg(line)
+
+    # bad hack :-(
+    time = timestamp.replace("'", "")
+    date_obj = datetime.fromtimestamp(float(time[:-3])) # timestamp has milliseconds, shave em off
+    nearest_minute = date_obj - timedelta(minutes=date_obj.minute % 1, seconds=date_obj.second, microseconds=date_obj.microsecond)
+
+    yield (nearest_minute, {'unique_id': uid, 'query': query, 'frequency': frequency})
--- search_terms/mapreduce.py
+++ search_terms/mapreduce.py
@@ -1,21 +1,33 @@
 import sys
 from disco.core import Disco, result_iterator
 from disco.settings import DiscoSettings
+
+from disco import func
 import time
 
 
 from mapper import map
 from reducer import reduce
 
-name = "ash-%s" % int(time.time())
+name = "gap-%s" % int(time.time())
 disco = Disco(DiscoSettings()['DISCO_MASTER'])
 print "Starting Disco job (%s).." % name
 print "Go to %s to see status of the job." % disco.master
+
+"""
+:clicks (ad id,people who clicked the ads)
+"""
 results = disco.new_job(name=name,
-                        input=["tag://data:foobar"],
-                        map=map,
-                        reduce=reduce,
-                        save=True).wait()
+        input=["tag://hackreduce:search:history"],
+        map_input_stream=(
+            func.map_input_stream,
+            func.chain_reader,
+        ),
+        map=map, 
+        reduce=reduce, 
+        save=True).wait()
+
 print "Job done. Results:"
 for word, count in result_iterator(results):
     print word, count
+
