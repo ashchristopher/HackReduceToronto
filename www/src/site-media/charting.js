2f134f249bcd8dc72898b5ce8b6861e03b26fe9a
Ash Christopher <ash.christopher@gmail.com>
2011-06-18 17:18:08 -0400

    updated js

--- www/src/site-media/charting.js
+++ www/src/site-media/charting.js
@@ -1,27 +1,66 @@
-var chart1; // globally available
+var chart;
 $(document).ready(function() {
-      chart1 = new Highcharts.Chart({
-         chart: {
-            renderTo: 'chart-container-1',
-            defaultSeriesType: 'bar'
-         },
+   chart = new Highcharts.Chart({
+      chart: {
+         renderTo: 'placeholder',
+         defaultSeriesType: 'column'
+      },
+      title: {
+         text: 'Search by Type'
+      },
+      xAxis: {
+         categories: [
+            'First', 
+            'Second', 
+            'Third' 
+         ]
+      },
+      yAxis: {
+         min: 0,
          title: {
-            text: 'Fruit Consumption'
-         },
-         xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-         },
-         yAxis: {
-            title: {
-               text: 'Fruit eaten'
-            }
-         },
-         series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-         }, {
-            name: 'John',
-            data: [5, 7, 3]
-         }]
-      });
+            text: 'Rainfall (mm)'
+         }
+      },
+      legend: {
+         layout: 'vertical',
+         backgroundColor: '#FFFFFF',
+         align: 'left',
+         verticalAlign: 'top',
+         x: 100,
+         y: 70,
+         floating: true,
+         shadow: true
+      },
+      tooltip: {
+         formatter: function() {
+            return ''+
+               this.x +': '+ this.y +' mm';
+         }
+      },
+      plotOptions: {
+         column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+         }
+      },
+           series: [{
+         name: 'Travel',
+         data: [49.9, 71.5, 106.4]
+   
+      }, {
+         name: 'Sex',
+         data: [83.6, 78.8, 98.5]
+   
+      }, {
+         name: 'Nerd',
+         data: [48.9, 38.8, 39.3]
+   
+      }, {
+         name: 'Cooking',
+         data: [42.4, 33.2, 34.5]
+   
+      }]
    });
+   
+   
+});
