a5852b4b8e5c29d03184279510606e580e0740ea
Ash Christopher <ash.christopher@gmail.com>
2011-06-18 16:55:15 -0400

    Updated

--- www/src/urls.py
+++ www/src/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,4 +13,5 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     url(r'^', include('mapreduce.urls')),
+    url(r'^site-media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root' : settings.MEDIA_ROOT }, name='media'),
 )
