e7a54255c5d2384c9a7d92dcf225ee1c29fd0885
Ash Christopher <ash.christopher@gmail.com>
2011-06-18 16:00:06 -0400

    Updated words

--- search_terms/words.py
+++ search_terms/words.py
@@ -1,1 +1,6 @@
 sexual = ('sex', 'tits', 'naked', 'girls', 'fuck', 'suck', 'teen', 'hot', 'cum', )
+
+programmer_nerd = (
+    'java ', 'c ', 'c++', 'php', 'visual basic', 'perl', 'python', 'c#', 'javascript', 'ruby', 
+    'erlang', 'lisp',
+)
